6a00a592e15b6329e4f098741b45fe7dc243c06c
akhilmantha <akhilmantha@live.com>
2018-02-02 12:06:22 +0530

    minor changes in the IBOutlets

--- Watch Extension/FlightInterfaceController.swift
+++ Watch Extension/FlightInterfaceController.swift
@@ -1,8 +1,8 @@
 //
 //  FlightInterfaceController.swift
-//  Watch Extension
+//  AirAber
 //
-//  Created by akhil mantha on 01/02/18.
+//  Created by akhil mantha on 02/02/18.
 //  Copyright © 2018 Mic Pringle. All rights reserved.
 //
 
@@ -13,39 +13,40 @@ import Foundation
 class FlightInterfaceController: WKInterfaceController {
   
   
-  @IBOutlet var flightLabel : WKInterfaceLabel!
-  @IBOutlet var routeLabel : WKInterfaceLabel!
-  @IBOutlet var boardingLabel : WKInterfaceLabel!
-  @IBOutlet var boardTimeLabel : WKInterfaceLabel!
-  @IBOutlet var statusLabel : WKInterfaceLabel!
-  @IBOutlet var gateLabel : WKInterfaceLabel!
-  @IBOutlet var seatLabel : WKInterfaceLabel!
-  
-  
-  var flight : Flight? {
-    
+  @IBOutlet var flightLabel: WKInterfaceLabel!
+  @IBOutlet var routeLabel: WKInterfaceLabel!
+  @IBOutlet var boardingLabel: WKInterfaceLabel!
+  @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+  @IBOutlet var statusLabel: WKInterfaceLabel!
+  @IBOutlet var gateLabel: WKInterfaceLabel!
+  @IBOutlet var seatLabel: WKInterfaceLabel!
+
+  var flight: Flight? {
+    // 2
     didSet {
-      
-      guard let flight  = flight else {return}
-      
-      flightLabel.setText("Flight \(flight.shortNumber) ")
+      // 3
+      guard let flight = flight else { return }
+      // 4
+      flightLabel.setText("Flight \(flight.shortNumber)")
       routeLabel.setText(flight.route)
       boardingLabel.setText("\(flight.number) Boards")
-      
-      
-      
-      
+      boardTimeLabel.setText(flight.boardsAt)
+      // 5
+      if flight.onSchedule {
+        statusLabel.setText("On Time")
+      } else {
+        statusLabel.setText("Delayed")
+        statusLabel.setTextColor(.red)
+      }
+      gateLabel.setText("Gate \(flight.gate)")
+      seatLabel.setText("Seat \(flight.seat)")
     }
-    
-    
-    
-    
-    
   }
-  
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+      
+      flight = Flight.allFlights().first
         
         // Configure interface objects here.
     }
